d5879fb2982f62b5c63344c6977daf414806eb9d
Rod <rod@Rods-iMac.local>
2024-10-20 20:09:43 -0400

    array Methods _ created class ProductManager with types / Created Class Validator as a private part of ProductManager

--- src/ArrayMethods/addDeleteMethods/ProductManager.ts
+++ src/ArrayMethods/addDeleteMethods/ProductManager.ts
@@ -1,24 +1,57 @@
-import { IProductManager } from './ProductManagerTypes';
+import { IErrorHandler, IExistsObj, IProduct, IProductManager, IValidator } from './ProductManagerTypes';
 
-const ProductManager: IProductManager = {
-  products: [],
-  success: false,
-  error: '',
+class ProductManager implements IProductManager {
+  private products: IProduct[];
+  private validator: IValidator;
 
-  addOneProduct(product) {
-    return this;
-  },
-  addManyProducts(arr) {
-    console.log(arr);
-    return this;
-  },
-  updateProduct(id, data) {
-    console.log(id, data);
-    return this;
-  },
-  getProducts() {
+  constructor() {
+    this.validator = new Validator();
+    this.products = [
+      { id: 1, name: 'Laptop', price: 1200 },
+      { id: 2, name: 'Desktop', price: 1300 },
+      { id: 3, name: 'Tablet', price: 1500 },
+    ];
+  }
+  addOneProduct(product: IProduct): this {
+    const isValid = this.validator.objectInArray(product, this.products) as IExistsObj;
+    if (!isValid.exists) {
+      this.products.push(product);
+      return this;
+    }
+    throw this.validator.handleError(isValid);
+  }
+  getAllProducts(): IProduct[] {
     return this.products;
-  },
-};
+  }
+}
+
+class Validator implements IValidator {
+  private result: IExistsObj;
+
+  constructor() {
+    this.result = { exists: false, errorMessage: '', index: 0 };
+  }
+  objectInArray<T extends IProduct>(obj: T, arr: T[]): IExistsObj | this {
+    const { id, name } = obj;
+    const index = arr.findIndex((prd) => id === prd.id);
+
+    if (index !== -1) {
+      this.result.exists = true;
+      this.result.errorMessage = `Product ${name} with id ${id} is already exists!`;
+      this.result.index = index;
+      return this.result;
+    }
+    return this;
+  }
+  handleError<T extends IExistsObj>(err: T): IErrorHandler {
+    const customError = new Error() as IErrorHandler;
+    const { exists, errorMessage, index } = err;
+    customError.message = errorMessage;
+    customError.index = index;
+    customError.exists = exists;
+
+    return customError;
+  }
+}
 
 export default ProductManager;
--- src/ArrayMethods/addDeleteMethods/ProductManagerTypes.ts
+++ src/ArrayMethods/addDeleteMethods/ProductManagerTypes.ts
@@ -1,16 +1,23 @@
+export interface IValidator {
+  objectInArray<T extends IProduct>(obj: T, arr: T[]): IExistsObj | this;
+  handleError<T extends IExistsObj>(err: T): IErrorHandler;
+}
+export interface IProductManager {
+  addOneProduct(product: IProduct): this | IErrorHandler;
+  getAllProducts(): IProduct[];
+}
+
+export interface IExistsObj {
+  exists: boolean;
+  errorMessage: string;
+  index: number | null;
+}
+
+export interface IErrorHandler extends Error, IExistsObj {}
+
 export interface IProduct {
   id: number;
   name: string;
   price: number;
-  description?: string;
-}
-
-export interface IProductManager {
-  products: IProduct[];
-  success: boolean;
-  error: string;
-  addOneProduct(product: IProduct): this;
-  addManyProducts(arr: IProduct[]): this;
-  updateProduct(id: number, data: Partial<IProduct>): this;
-  getProducts(): IProduct[];
+  [key: string]: string | number | boolean | [{ [key: string]: string | number | boolean }];
 }
