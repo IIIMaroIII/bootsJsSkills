93243b80c42fe89711762aa02e8aa8c93b0c1764
Rodion Matvieiev <iiimaroiii@IIIMaroIIIs-Mac-Pro.local>
2024-11-04 22:44:35 -0500

    encupsulated the logic and made an reexport

--- src/Validator/Validator.ts
+++ src/Validator/Validator.ts
@@ -1,78 +1,26 @@
-import { IErrors, IValidator } from './ValidatorTypes';
+import { IInfo, IValidator } from './ValidatorTypes';
 
 class Validator implements IValidator {
-  protected errors: IErrors;
+  protected info: IInfo;
 
   constructor() {
-    this.errors = { isValid: true, errorMessages: [] };
+    this.info = { isValid: true, errorMessages: [], type: null };
   }
-  addError(message: string): this {
-    this.errors.isValid = false;
-    this.errors.errorMessages.push(message);
+  protected addError(message: string): this {
+    this.info.isValid = false;
+    this.info.errorMessages.push(message);
     return this;
   }
-  getErrors(): IErrors {
-    console.log('this.errors', this.errors);
-    return this.errors;
+
+  getErrors(): string[] | undefined {
+    if (this.info.errorMessages.length === 0) {
+      return undefined;
+    }
+    return this.info.errorMessages;
+  }
+  getInfo(): IInfo {
+    return this.info;
   }
 }
 
-class StringValidator<T> extends Validator {
-  private value: T;
-
-  constructor(value: T) {
-    super();
-    this.value = value;
-  }
-
-  minLength(v: number, message: string = `Min length is ${v} symbols`): this {
-    console.log('this.errors', this.errors);
-    console.log('this.value in min()', this.value);
-    console.log('v parametr in min()', v);
-    console.log('optional message parametr', message);
-    return this;
-  }
-  maxLength(v: number, message: string = `Max length is ${v} symbols`): this {
-    console.log('this.value in max()', this.value);
-    console.log('v parametr in max()', v);
-    console.log('optional message parametr in max()', message);
-    return this;
-  }
-}
-
-class NumberValidator<T> extends Validator {
-  private value: T;
-
-  constructor(value: T) {
-    super();
-    this.value = value;
-  }
-
-  odd(v: number, message: string = 'Must be odd'): this {
-    console.log('this.errors in odd()', this.errors);
-    console.log('this.value in odd()', this.value);
-    console.log('v in odd()', v);
-    console.log('message in odd()', message);
-    return this;
-  }
-  even(v: number, message: string = 'Must be even'): this {
-    console.log('this.errors in even()', this.errors);
-    console.log('this.value in even()', this.value);
-    console.log('v in even()', v);
-    console.log('message in even()', message);
-    return this;
-  }
-}
-
-function validate(value: string): StringValidator<string>;
-function validate(value: number): NumberValidator<number>;
-function validate(value: unknown): StringValidator<string> | NumberValidator<number> | IErrors {
-  if (typeof value === 'string') {
-    return new StringValidator(value);
-  }
-  if (typeof value === 'number') {
-    return new NumberValidator(value);
-  }
-  return new Validator().addError('Unsupported type').getErrors();
-}
-export default validate;
+export default Validator;
